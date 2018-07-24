1ca62363c6ceebecd050f399bfdca80433364712
[OSC] Sven Schliesing <s.schliesing@ndr.de>
2018-07-24 14:01:32 +0200

    Post certificate to Pulse Secure VTM via curl
    Aims to be as portable as possible. No dependency apart from bash and
    curl

---
+++ hooks.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+function deploy_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+}
+
+function clean_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+}
+
+deploy_cert() {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
+
+    FULLCHAIN=`cat ${FULLCHAINFILE}`
+    KEY=`cat ${KEYFILE}`
+
+    JSON="{
+        \"properties\": {
+            \"basic\": {
+                \"note\": \"${DOMAIN}\",
+                \"private\": \"${KEY}\",
+                \"public\": \"${FULLCHAIN}\",
+            }
+        }
+    }";
+
+    echo "$JSON" > cert.json
+
+    curl -u usr:pwd -k --data-binary @cert.json -H "Content-Type: application/json" -X PUT https://my-vtm:9070/api/tm/5.1/config/active/ssl/server_keys/mycert
+
+    rm cert.json
+}
+
+function unchanged_cert {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+}
+
+invalid_challenge() {
+    local DOMAIN="${1}" RESPONSE="${2}"
+}
+
+request_failure() {
+    local STATUSCODE="${1}" REASON="${2}" REQTYPE="${3}"
+}
+
+startup_hook() {
+  :
+}
+
+exit_hook() {
+  :
+}
